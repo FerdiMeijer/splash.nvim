584db256931721aae3c1f552b90c94645f79427e
FerdiMeijer <ferdi_meijer@hotmail.com>
2026-04-22 13:35:25 +0200

    fix: hide lualine during splash by setting statusline = 0

--- lua/splash/current.lua
+++ lua/splash/current.lua
@@ -32,6 +32,14 @@ end
 M.override_win_opts = function()
 	M.window = vim.api.nvim_get_current_win()
 	M.originals = get_window_options(M.window, overrides)
+
+	-- Save and hide statusline (delayed to override lualine and other statusline plugins)
+	M.original_laststatus = vim.o.laststatus
+	vim.schedule(function()
+		vim.o.laststatus = 0
+		log.debug("Hiding statusline (original laststatus: " .. M.original_laststatus .. ")")
+	end)
+
 	log.debug("Applying window: " .. M.window .. " options: " .. vim.inspect(overrides))
 	set_window_options(M.window, overrides)
 end
@@ -39,6 +47,12 @@ end
 M.restore_win_opts = function()
 	log.debug("Restoring window: " .. M.window .. " options: " .. vim.inspect(M.originals))
 	set_window_options(M.window, M.originals)
+
+	-- Restore statusline
+	if M.original_laststatus then
+		vim.o.laststatus = M.original_laststatus
+		log.debug("Restored statusline (laststatus: " .. M.original_laststatus .. ")")
+	end
 end
 
 return M
